23f1170af18abe8a18fda1c80f42cfbd1ded7984
Nikita <azazloker.dadaya@gmail.com>
2022-12-21 21:19:29 +0300

    КОРСЫ ы

--- src/main/java/com/example/booking/config/SimpleCORSFilter.java
+++ src/main/java/com/example/booking/config/SimpleCORSFilter.java
@@ -1,24 +1,28 @@
 package com.example.booking.config;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 
-@Component
-public class SimpleCORSFilter extends OncePerRequestFilter {
-    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+@Configuration
+public class SimpleCORSFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "authorization,Content-Type,x-requested-with,xsrf-token");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
-        filterChain.doFilter(request, response);
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Request-Headers","Content-Type, x-requested-with");
+        chain.doFilter(req, res);
     }
+    @Override
+    public void init(FilterConfig filterConfig) {}
+    @Override
+    public void destroy() {}
 }
 
