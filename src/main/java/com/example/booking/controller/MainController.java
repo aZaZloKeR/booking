f461e84439b776bbb1e2fe5161023433d50bcbd0
Nikita <azazloker.dadaya@gmail.com>
2022-12-21 23:10:33 +0300

    проверка пароля

--- src/main/java/com/example/booking/controller/MainController.java
+++ src/main/java/com/example/booking/controller/MainController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Objects;
 import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:4200")
@@ -34,14 +35,19 @@ public class MainController {
         System.out.println("login = " + guest.getLogin() + ", password = "+ guest.getPassword());
         Optional<Guest> byLogin = guestRepository.findByLogin(guest.getLogin());
         if(byLogin.isPresent()){
+
             Guest guest1 = byLogin.get();
-            guest1.setRole("guest");
-            return guest1;
+            if(Objects.equals(guest1.getPassword(), guest.getPassword())){
+                guest1.setRole("guest");
+                return guest1;
+            }
         }
         else if (administratorRepository.findByLogin(guest.getLogin()).isPresent()){
             Administrator administrator = administratorRepository.findByLogin(guest.getLogin()).get();
-            administrator.setRole("admin");
-            return administrator;
+            if(Objects.equals(administrator.getPassword(), guest.getPassword())){
+                administrator.setRole("admin");
+                return administrator;
+            }
         }
         return "-1";
     }
